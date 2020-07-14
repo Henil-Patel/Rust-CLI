f99ac36b39a8a42af850310ae164ce6adc1bac85
Henil <henil.patel@hotmail.com>
2020-07-14 10:45:15 -0400

    Project Init

---
+++ minigrep/target/debug/deps/minigrep-989ae2ed99327e97.d
@@ -0,0 +1,5 @@
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-989ae2ed99327e97: src/main.rs
+
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-989ae2ed99327e97.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/minigrep: /home/henil/Desktop/Rust-CLI/minigrep/src/main.rs
