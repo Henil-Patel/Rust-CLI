240201e089c78ab2d6cc10a8b933f777ca88d024
Henil-Patel <henil.patel@hotmail.com>
2020-09-04 09:40:42 -0400

    Finished minigrep tutorial in Rust

---
+++ minigrep/target/debug/deps/minigrep-3b1342567fa520b5.d
@@ -0,0 +1,5 @@
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-3b1342567fa520b5: src/main.rs
+
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-3b1342567fa520b5.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-dcf96294b5df8f71.d
@@ -0,0 +1,5 @@
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-dcf96294b5df8f71: src/lib.rs
+
+/home/henil/Desktop/Rust-CLI/minigrep/target/debug/deps/minigrep-dcf96294b5df8f71.d: src/lib.rs
+
+src/lib.rs:
